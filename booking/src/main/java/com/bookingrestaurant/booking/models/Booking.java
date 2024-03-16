9ea8971ec8535b2d5abfebc71186fd41fe4f9fef
ErickSot <108371429+ErickSot@users.noreply.github.com>
2024-03-15 19:05:34 -0600

    added getter and setters for Booking

--- booking/src/main/java/com/bookingrestaurant/booking/models/Booking.java
+++ booking/src/main/java/com/bookingrestaurant/booking/models/Booking.java
@@ -6,4 +6,22 @@ public class Booking {
     private String customerName;
     private int tableSize;
     private LocalDateTime dateTime;
+    public String getCustomerName() {
+        return customerName;
+    }
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+    public int getTableSize() {
+        return tableSize;
+    }
+    public void setTableSize(int tableSize) {
+        this.tableSize = tableSize;
+    }
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
 }
